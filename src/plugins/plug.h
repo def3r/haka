e13dd53f285ba4be3168d74aee6f5609ab11962c
horrifyingHorse <excuseplanation@gmail.com>
2026-01-25 14:28:52 +0530

    plugin example api

---
+++ src/plugins/plug.h
@@ -0,0 +1,76 @@
+#ifndef _PLUG_H
+#define _PLUG_H
+
+#include <linux/input-event-codes.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HAKA_ABI_VERSION 0x1
+
+extern const struct coreApi* api;
+
+struct hakaContext;
+struct keyBindings;
+struct keyState;
+
+// clang-format off
+struct coreApi {
+  int ver;
+
+  void   (*addKeyBind)(const struct keyBindings* kbinds,
+                void (*func)(struct hakaContext*),
+                int keyToBind,
+                ...);
+
+  void   (*switchFile)(struct hakaContext *);
+  void   (*getPrimarySelection)(struct hakaContext*, FILE* fp);
+  int    (*openNotesFile)(struct hakaContext*);
+  size_t (*writeFP2FD)(struct hakaContext*);
+  int    (*closeNotesFile)(struct hakaContext*);
+  void   (*writeTextToFile)(struct hakaContext *,
+                            char *prefix, char *suffix);
+  void   (*writeToFile)(struct hakaContext *);
+
+
+  void   (*openFile)(struct hakaContext *);
+
+  void   (*sendTextToFile)(struct hakaContext *,
+                           char *text);
+  void   (*triggerTofi)(struct hakaContext *);
+};
+// clang-format on
+
+int hakaPluginInit(const struct coreApi* capi,
+                   const struct keyBindings* kbinds);
+
+#define Validate(capi, kbinds)                             \
+  do {                                                     \
+    unsigned e = ((capi == NULL) << 1) | (kbinds == NULL); \
+    if (e & 0x2) {                                         \
+      fprintf(stderr, "Core API ptr is NULL\n");           \
+    }                                                      \
+    if (e & 0x1) {                                         \
+      fprintf(stderr, "Key binds ptr is NULL\n");          \
+    }                                                      \
+    if (e) {                                               \
+      return -1;                                           \
+    }                                                      \
+  } while (0)
+
+#define Bind(func, ...) api->addKeyBind(kbinds, func, __VA_ARGS__, 0)
+
+#define BEGIN_BIND                                       \
+  int hakaPluginInit(const struct coreApi* capi,         \
+                     const struct keyBindings* kbinds) { \
+    Validate(capi, kbinds);                              \
+    api = capi;                                          \
+    if (api->ver != HAKA_ABI_VERSION) {                  \
+      return -1;                                         \
+    }
+
+#define END_BIND \
+  return 0;      \
+  }
+
+#endif  // !_PLUG_H
---
+++ src/plugins/plugin.c
@@ -0,0 +1,21 @@
+#include "plug.h"
+
+const struct coreApi *api = NULL;
+
+static void writePointToFile(struct hakaContext *ctx) {
+  api->writeTextToFile(ctx, "- ", "");
+}
+
+static void writeSubPointToFile(struct hakaContext *ctx) {
+  api->writeTextToFile(ctx, "  - ", "");
+}
+
+static void sendNewlineToFile(struct hakaContext *ctx) {
+  api->sendTextToFile(ctx, "\n");
+}
+
+BEGIN_BIND
+  Bind(writePointToFile, KEY_GRAVE);
+  Bind(writeSubPointToFile, KEY_S);
+  Bind(sendNewlineToFile, KEY_N);
+END_BIND
